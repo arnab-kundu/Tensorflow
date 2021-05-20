dd868199ce2d27862f365c4deca6ffbd15df4ceb
arnab-kundu <arnab.kundu1990@gmail.com>
2021-05-20 21:00:55 +0530

    initial commit. Project created

---
+++ Android/LinearRegression/app/src/main/java/com/example/linearregression/LinearRegressionActivity.java
@@ -0,0 +1,14 @@
+package com.example.linearregression;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearRegressionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_regression);
+    }
+}
